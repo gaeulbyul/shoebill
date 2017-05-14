33628ef114d173e90bb3dd3c415b30650841e689
Gaeulbyul <aeiou256@gmail.com>
2017-05-14 17:34:37 +0900

    외부 링크 작동하도록 고침

--- src/renderer.js
+++ src/renderer.js
@@ -93,6 +93,18 @@ document.addEventListener('DOMContentLoaded', () => {
   });
   ipcRenderer.send('ipc.renderer.shoebill.config/request-load');
   const webview = document.getElementById('tweetdeck');
+  webview.addEventListener('new-window', event => {
+    const { url, frameName } = event;
+    try {
+      const parsedURL = URL.parse(url);
+      if (!(['http:', 'https:', 'mailto:'].includes(parsedURL.protocol))) {
+        throw new Error('Unknown protocol!');
+      }
+      shell.openExternal(url);
+    } catch (e) {
+      console.error('Error on opening url "%s"', url);
+    }
+  });
   webview.addEventListener('ipc-message', event => {
     const { channel, args } = event;
     if (channel === 'ipc.renderer.shoebill.ui/toast-message') {
